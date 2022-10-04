a1973c3845f6342fdac22bcb3733fde17c08ea62
AYESIGYE DERRICK <ayesigyederrick@gmail.com>
2022-10-04 15:18:37 +0300

    Added modifier() to change appearance of Text on screen
    This added Padding on Left, Top and Right

--- app/src/main/java/com/ayesigyederrick/happybirthday/MainActivity.kt
+++ app/src/main/java/com/ayesigyederrick/happybirthday/MainActivity.kt
@@ -11,11 +11,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ayesigyederrick.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -51,8 +53,19 @@ fun BirthdayGreetingWithImage(name:String, from: String){
 @Composable
 fun BirthdayGreetingWithText(name: String, from: String){
     Column {
-        Text(text = "Happy Birthday  $name", fontSize = 36.sp)
-        Text(text = from, fontSize = 24.sp)
+        Text(text = "Happy Birthday  $name",
+            fontSize = 36.sp,
+            modifier = Modifier.fillMaxWidth()
+                .wrapContentWidth(align = Alignment.Start)
+                .padding(start = 16.dp, top = 16.dp)
+        )
+        Text(text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth().wrapContentWidth(align = Alignment.End)
+                .padding(start = 16.dp, end = 16.dp)
+            )
+
     }
 }
 
