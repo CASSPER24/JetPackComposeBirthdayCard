71920111d2086a3df51c661faf171a309026910b
AYESIGYE DERRICK <ayesigyederrick@gmail.com>
2022-10-04 15:27:14 +0300

    Added Text on Screen i.e name and from, to string Resource File

--- app/src/main/java/com/ayesigyederrick/happybirthday/MainActivity.kt
+++ app/src/main/java/com/ayesigyederrick/happybirthday/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -73,6 +74,6 @@ fun BirthdayGreetingWithText(name: String, from: String){
 @Composable
 fun DefaultPreview() {
     HappyBirthdayTheme {
-        BirthdayGreetingWithImage("Derrick", "All the Best from Cassper")
+        BirthdayGreetingWithImage(stringResource(R.string.receiver_name), stringResource(R.string.from_text))
     }
 }
